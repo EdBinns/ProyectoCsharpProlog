00bf8fb90b98b0ee6bb782b4c9f7d6ea672713bd
EdBinns <eduardo.binns.sanchez@gmail.com>
2020-11-14 18:21:28 -0600

    Se llena el random

--- ConexionProlog/ConexionProlog/Principal.Designer.cs
+++ ConexionProlog/ConexionProlog/Principal.Designer.cs
@@ -34,33 +34,36 @@ namespace ConexionProlog
             this.tabla = new System.Windows.Forms.DataGridView();
             this.label1 = new System.Windows.Forms.Label();
             this.tbMatriz = new System.Windows.Forms.TextBox();
+            this.btnConsultas = new System.Windows.Forms.Button();
+            this.btnRandom = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.tabla)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(476, 595);
+            this.button1.Location = new System.Drawing.Point(16, 144);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(105, 28);
+            this.button1.Size = new System.Drawing.Size(222, 39);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Cargar";
+            this.button1.Text = "Crear Matriz";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // tabla
             // 
             this.tabla.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.tabla.Location = new System.Drawing.Point(12, 12);
+            this.tabla.Location = new System.Drawing.Point(244, 12);
             this.tabla.Name = "tabla";
             this.tabla.RowHeadersWidth = 62;
             this.tabla.RowTemplate.Height = 28;
-            this.tabla.Size = new System.Drawing.Size(1008, 533);
+            this.tabla.Size = new System.Drawing.Size(1200, 750);
             this.tabla.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(21, 595);
+            this.label1.Location = new System.Drawing.Point(12, 68);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(226, 20);
             this.label1.TabIndex = 3;
@@ -69,23 +72,57 @@ namespace ConexionProlog
             // 
             // tbMatriz
             // 
-            this.tbMatriz.Location = new System.Drawing.Point(253, 592);
+            this.tbMatriz.Location = new System.Drawing.Point(16, 100);
             this.tbMatriz.Name = "tbMatriz";
-            this.tbMatriz.Size = new System.Drawing.Size(193, 26);
+            this.tbMatriz.Size = new System.Drawing.Size(222, 26);
             this.tbMatriz.TabIndex = 4;
             this.tbMatriz.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // btnConsultas
+            // 
+            this.btnConsultas.Location = new System.Drawing.Point(16, 278);
+            this.btnConsultas.Name = "btnConsultas";
+            this.btnConsultas.Size = new System.Drawing.Size(222, 39);
+            this.btnConsultas.TabIndex = 5;
+            this.btnConsultas.Text = "Consultas";
+            this.btnConsultas.UseVisualStyleBackColor = true;
+            this.btnConsultas.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnRandom
+            // 
+            this.btnRandom.Location = new System.Drawing.Point(16, 232);
+            this.btnRandom.Name = "btnRandom";
+            this.btnRandom.Size = new System.Drawing.Size(222, 40);
+            this.btnRandom.TabIndex = 6;
+            this.btnRandom.Text = "LLenar tabla random";
+            this.btnRandom.UseVisualStyleBackColor = true;
+            this.btnRandom.Click += new System.EventHandler(this.btnRandom_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(16, 189);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(222, 37);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Fin llenado manual";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
             // 
             // Principal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1033, 632);
+            this.ClientSize = new System.Drawing.Size(1456, 774);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnRandom);
+            this.Controls.Add(this.btnConsultas);
             this.Controls.Add(this.tbMatriz);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.tabla);
             this.Controls.Add(this.button1);
             this.Name = "Principal";
             this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.Principal_Load);
             ((System.ComponentModel.ISupportInitialize)(this.tabla)).EndInit();
             this.ResumeLayout(false);
@@ -99,6 +136,9 @@ namespace ConexionProlog
         private System.Windows.Forms.DataGridView tabla;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox tbMatriz;
+        private System.Windows.Forms.Button btnConsultas;
+        private System.Windows.Forms.Button btnRandom;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- ConexionProlog/ConexionProlog/Principal.cs
+++ ConexionProlog/ConexionProlog/Principal.cs
@@ -13,41 +13,57 @@ namespace ConexionProlog
 {
 	public partial class Principal : Form
 	{
+		Boolean flag = false;
+		DataTable dataTable = new DataTable();
+		int tamannoMatriz = 0;
 		public Principal()
 		{
 			InitializeComponent();
 			this.CenterToScreen();
 			this.Text = "Proyecto C# conectado con Prolog";
+			
 		}
 
         private void Principal_Load(object sender, EventArgs e)
         {
 			Environment.SetEnvironmentVariable("SWI_HOME_DIR", @"C:\Program Files (x86)\swipl");
 			Environment.SetEnvironmentVariable("Path", @"C:\Program Files (x86)\swipl\bin");
-			string[] p = { "-q", "-f", @"practica.pl" };
+			string[] p = { "-q", "-f", @"proyecto.pl" };
 
 			// Connect to Prolog Engine
 			PlEngine.Initialize(p);
+		
+			tabla.CellClick += listenerrDataGridViewOnClick;
+			tabla.ColumnHeadersVisible = false;
+			tabla.RowHeadersVisible = false;
+			tabla.AllowUserToAddRows = false;
+			tabla.AllowUserToResizeRows = false;
+			tabla.AllowUserToResizeColumns = false;
+			tabla.AllowDrop = false;
 
 		}
 
         private void button1_Click(object sender, EventArgs e)
         {
 
-		
 
-			PlQuery consulta = new PlQuery("max(6,5,X)");
+			dataTable.Rows.Clear();
+			dataTable.Columns.Clear();
+
+			PlQuery consulta = new PlQuery("punto(2,X)");
 
 			foreach (PlQueryVariables z in consulta.SolutionVariables)
 				Console.WriteLine(z["X"].ToString());
-			//listBox1.Items.Add();	
+			
+
+			flag = false;
 
 
-			var dataTable = new DataTable();
-			var tamannoMatriz = int.Parse(tbMatriz.Text.ToString());
+			
+			tamannoMatriz = int.Parse(tbMatriz.Text.ToString());
 			for (int i = 0; i < tamannoMatriz; i++)
 			{
-				dataTable.Columns.Add(" " + i);
+				dataTable.Columns.Add();
 			}
 
 			for (int i = 0; i < tamannoMatriz; i++)
@@ -56,8 +72,7 @@ namespace ConexionProlog
 			}
 
 			tabla.DataSource = dataTable;
-			tabla.CellClick += listenerrDataGridViewOnClick;
-			Console.WriteLine(dataTable.Rows[0]);
+
 		}
 
         private void label1_Click(object sender, EventArgs e)
@@ -66,17 +81,68 @@ namespace ConexionProlog
         }
 		private void listenerrDataGridViewOnClick(object sender, DataGridViewCellEventArgs e)
 		{
-			DataGridViewCell cell = (DataGridViewCell)tabla.Rows[e.RowIndex].Cells[e.ColumnIndex];
-			Console.WriteLine(cell.Value);
-			Console.WriteLine("Funciona");
-			if (cell.Value == "sewagram express")
+           DataGridViewCell cell = (DataGridViewCell)tabla.Rows[e.RowIndex].Cells[e.ColumnIndex];
+		   Console.WriteLine(cell.Value);
+		   Console.WriteLine("Funciona");
+		   if (flag == true)
+		    {
+				cell.Style.BackColor = System.Drawing.Color.Red;
+		    }
+		   else
 			{
-			
-			}
+				if (cell.Value == "O")
+				{
+					cell.Value = " ";
+				}
+				else
+				{
+					cell.Value = "O";
+				}
+			}		
 		}
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
+		
+		}
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRandom_Click(object sender, EventArgs e)
+        {
+	
+
+			Random rnd = new Random();
+			int totalValues = rnd.Next(0, tamannoMatriz*tamannoMatriz);
+
+			for (int i = 0; i < tamannoMatriz; i++)
+			{
+				for (int j = 0; j < tamannoMatriz; j++)
+				{
+					dataTable.Rows[i][j] = " ";
+				}
+			}
+
+			Console.WriteLine(totalValues);
+			Console.WriteLine(tamannoMatriz);
+			for (int i = 0; i <= totalValues; i++)
+			{
+				int colums = rnd.Next(0, tamannoMatriz );
+				int rows = rnd.Next(0, tamannoMatriz);
+				dataTable.Rows[colums][rows] = "O";
+			}
+
+		
+				
+			
+		}
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+			flag = true;
 
         }
     }
