4fb3d8ec41ae0079b3d2d448310d09b03cef6cb4
EdBinns <eduardo.binns.sanchez@gmail.com>
2020-11-25 21:33:35 -0600

    Prueba

--- ConexionProlog/ConexionProlog/bin/Debug/bdp.pl
+++ ConexionProlog/ConexionProlog/bin/Debug/bdp.pl
@@ -1,1 +1,17 @@
 :- dynamic punto/2.
+punto(3,0).
+punto(3,2).
+punto(0,1).
+punto(0,3).
+punto(1,2).
+punto(4,2).
+punto(1,1).
+punto(4,2).
+punto(0,3).
+punto(0,4).
+punto(0,0).
+punto(2,0).
+punto(0,4).
+punto(2,3).
+punto(3,4).
+punto(3,3).
